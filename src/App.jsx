7de601c1407a90767d675dc454ed11760e2f03be
Hanhtunaung97 <littlehanhtunaung97>
2024-06-24 08:34:02 +0800

    route path set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import RegisterPage from './page/Register.page'
+import LoginPage from './page/Login.page'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <Routes>
+        <Route path='/' element={<RegisterPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+      </Routes>
+    </main>
   )
 }
 
