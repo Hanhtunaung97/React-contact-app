08d0368415029148231def2845b179daf118fd65
Hanhtunaung97 <littlehanhtunaung97>
2024-06-24 09:14:04 +0800

    register page set up

--- src/components/Button.components.jsx
+++ src/components/Button.components.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const ButtonComponents = ({children}) => {
+const ButtonComponents = ({children,style,...rest}) => {
   return (
-    <button className=' text-purple-50 bg-purple-500 px-3 py-2 border border-purple-300 rounded-lg duration-200 active:scale-90 active:bg-purple-50 active:text-purple-500 w-1/2 mx-auto'>{children}</button>
+    <button {...rest} className={`text-purple-50 bg-purple-500 px-3 py-2 border border-purple-300 rounded-lg duration-200 active:scale-90 active:bg-purple-50 active:text-purple-500 w-1/2 mx-auto ${style}`}>{children}</button>
   )
 }
 
--- src/page/Register.page.jsx
+++ src/page/Register.page.jsx
@@ -1,23 +1,70 @@
-import React from 'react'
-import { ButtonComponents, ContainerComponents, InputFormComponents } from '../components'
+import React, { useState } from "react";
+import {
+  ButtonComponents,
+  ContainerComponents,
+  InputFormComponents,
+} from "../components";
 
 const RegisterPage = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+  });
+  const handleInputChange = (e) => {
+    setFormData((pre) => ({ ...pre, [e.target.name]: e.target.value }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
     <ContainerComponents>
-        <div className='Center'>
-            <div className=' w-2/6 h-auto space-y-7'>
-                <h1 className=' font-heading text-xl md:text-3xl font-bold text-purple-700 text-center'>Register Your Contact</h1>
-                <form className=' flex flex-col gap-y-5' >
-                   <InputFormComponents type={"name"} name={"name"} label={"Enter UserName :"} placeholder='Your Name'/>
-                   <InputFormComponents type={"email"} name={"email"} label={"Enter Your Email :"} placeholder='example@gamil.com'/>
-                   <InputFormComponents type={"password"} name={"password"} label={"Password :"} placeholder='Password'/>
-                   <InputFormComponents type={"password"} name={"password_confirmation"} label={"Confirm Password :"} placeholder='Password Confirm'/>
-                   <ButtonComponents>Register</ButtonComponents>
-                </form>
-            </div>
+      <div className="Center">
+        <div className=" w-2/6 h-auto space-y-7">
+          <h1 className=" font-heading text-xl md:text-3xl font-bold text-purple-700 text-center">
+            Register Your Contact
+          </h1>
+          <form onSubmit={handleSubmit} className=" flex flex-col gap-y-5">
+            <InputFormComponents
+              onChange={handleInputChange}
+              value={formData.name}
+              type={"name"}
+              name={"name"}
+              label={"Enter UserName :"}
+              placeholder="Your Name"
+            />
+            <InputFormComponents
+              onChange={handleInputChange}
+              value={formData.email}
+              type={"email"}
+              name={"email"}
+              label={"Enter Your Email :"}
+              placeholder="example@gamil.com"
+            />
+            <InputFormComponents
+              onChange={handleInputChange}
+              value={formData.password}
+              type={"password"}
+              name={"password"}
+              label={"Password :"}
+              placeholder="Password"
+            />
+            <InputFormComponents
+              onChange={handleInputChange}
+              value={formData.password_confirmation}
+              type={"password"}
+              name={"password_confirmation"}
+              label={"Confirm Password :"}
+              placeholder="Password Confirm"
+            />
+            <ButtonComponents type="submit">Register</ButtonComponents>
+          </form>
         </div>
+      </div>
     </ContainerComponents>
-  )
-}
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
