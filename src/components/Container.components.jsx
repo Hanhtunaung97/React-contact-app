afdedfd505b8ac4bc78d5084909d2159950e9329
Hanhtunaung97 <littlehanhtunaung97>
2024-06-24 08:42:46 +0800

    register page view start

--- src/components/Container.components.jsx
+++ src/components/Container.components.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContainerComponents = ({children}) => {
   return (
-    <div className=' max-w-[1000px] mx-auto min-h-screen'>{children}</div>
+    <div className=' max-w-[1000px] mx-auto h-screen'>{children}</div>
   )
 }
 
--- src/page/Register.page.jsx
+++ src/page/Register.page.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { ContainerComponents } from '../components'
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <ContainerComponents>
+        <div className='Center'>
+            <div className=' w-2/6 h-auto'>
+                <h1 className=' font-heading text-xl md:text-4xl font-bold text-purple-700'>Register Your Contact</h1>
+                
+            </div>
+        </div>
+    </ContainerComponents>
   )
 }
 
