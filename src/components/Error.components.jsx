008d07a037b0de027f6b41732380df3fffbb747a
Hanhtunaung97 <littlehanhtunaung97>
2024-06-27 12:52:26 +0800

    register page data fetching complete

--- src/components/Error.components.jsx
+++ src/components/Error.components.jsx
@@ -3,7 +3,7 @@ import { IoWarning } from "react-icons/io5";
 
 const ErrorComponents = ({children}) => {
   return (
-    <div className=' text-red-600 flex justify-center items-center py-3 border shadow gap-x-5 my-5'>
+    <div className=' text-red-600 flex justify-center items-center py-3 border shadow gap-x-3 my-5'>
         <IoWarning />
         <h1>{children}</h1>
     </div>
--- src/page/Register.page.jsx
+++ src/page/Register.page.jsx
@@ -1,85 +1,94 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   ButtonComponents,
   ContainerComponents,
   InputFormComponents,
+  LoadingComponents,
 } from "../components";
 import { useNavigate } from "react-router-dom";
 import useApi from "../hook/useApi";
 import { Register } from "../service/auth.service";
+import ErrorComponents from "../components/Error.components";
 
 const RegisterPage = () => {
-  const {dealingApi,loading,error,data}=useApi(Register)
-  const nav=useNavigate();
+  const { dealingApi, loading, error, data } = useApi(Register);
+  const nav = useNavigate();
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     password: "",
     password_confirmation: "",
   });
-
+  useEffect(() => {
+    data && nav("/")
+  },[data])
   const handleInputChange = (e) => {
     setFormData((pre) => ({ ...pre, [e.target.name]: e.target.value }));
   };
   const handleSubmit = (e) => {
     e.preventDefault();
     // console.log(formData);
-    dealingApi(formData)
+    dealingApi(formData);
   };
-  console.log(loading,error,data);
+  console.log(loading, error, data);
   return (
     <ContainerComponents>
-      <div className="Center">
-        <div className=" w-2/6 h-auto space-y-7">
-          <h1 className=" font-heading text-xl md:text-3xl font-bold text-purple-700 text-center">
-            Register Your Contact
-          </h1>
-          <form onSubmit={handleSubmit} className=" flex flex-col gap-y-5">
-            <InputFormComponents
-              onChange={handleInputChange}
-              value={formData.name}
-              type={"name"}
-              name={"name"}
-              label={"Enter UserName :"}
-              placeholder="Your Name"
-            />
-            <InputFormComponents
-              onChange={handleInputChange}
-              value={formData.email}
-              type={"email"}
-              name={"email"}
-              label={"Enter Your Email :"}
-              placeholder="example@gamil.com"
-            />
-            <InputFormComponents
-              onChange={handleInputChange}
-              value={formData.password}
-              type={"password"}
-              name={"password"}
-              label={"Password :"}
-              placeholder="Password"
-            />
-            <InputFormComponents
-              onChange={handleInputChange}
-              value={formData.password_confirmation}
-              type={"password"}
-              name={"password_confirmation"}
-              label={"Confirm Password :"}
-              placeholder="Password Confirm"
-            />
-            <ButtonComponents type="submit">Register</ButtonComponents>
-          </form>
-          <p className=" text-purple-500 font-heading">
-            You have already account ! plz login{" "}
-            <button
-              onClick={() => nav("/")}
-              className=" underline active:underline-offset-4 duration-300 font-semibold active:scale-90"
-            >
-              Login
-            </button>
-          </p>
+      {loading ? (
+        <LoadingComponents />
+      ) : (
+        <div className="Center">
+          <div className=" w-2/5 h-auto space-y-5">
+            <h1 className=" font-heading text-xl md:text-3xl font-bold text-purple-700 text-center">
+              Register Your Contact
+            </h1>
+            {error && <ErrorComponents>{error}</ErrorComponents>}
+            <form onSubmit={handleSubmit} className=" flex flex-col gap-y-5">
+              <InputFormComponents
+                onChange={handleInputChange}
+                value={formData.name}
+                type={"name"}
+                name={"name"}
+                label={"Enter UserName :"}
+                placeholder="Your Name"
+              />
+              <InputFormComponents
+                onChange={handleInputChange}
+                value={formData.email}
+                type={"email"}
+                name={"email"}
+                label={"Enter Your Email :"}
+                placeholder="example@gamil.com"
+              />
+              <InputFormComponents
+                onChange={handleInputChange}
+                value={formData.password}
+                type={"password"}
+                name={"password"}
+                label={"Password :"}
+                placeholder="Password"
+              />
+              <InputFormComponents
+                onChange={handleInputChange}
+                value={formData.password_confirmation}
+                type={"password"}
+                name={"password_confirmation"}
+                label={"Confirm Password :"}
+                placeholder="Password Confirm"
+              />
+              <ButtonComponents type="submit">Register</ButtonComponents>
+            </form>
+            <p className=" text-purple-500 font-heading">
+              You have already account ! plz login{" "}
+              <button
+                onClick={() => nav("/")}
+                className=" underline active:underline-offset-4 duration-300 font-semibold active:scale-90"
+              >
+                Login
+              </button>
+            </p>
+          </div>
         </div>
-      </div>
+      )}
     </ContainerComponents>
   );
 };
