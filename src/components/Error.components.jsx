404f1e4ab9bd211f0b09616d5f054929ac31f15e
Hanhtunaung97 <littlehanhtunaung97>
2024-06-27 12:43:46 +0800

    error components set up

---
+++ src/components/Error.components.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { IoWarning } from "react-icons/io5";
+
+const ErrorComponents = ({children}) => {
+  return (
+    <div className=' text-red-600 flex justify-center items-center py-3 border shadow gap-x-5 my-5'>
+        <IoWarning />
+        <h1>{children}</h1>
+    </div>
+  )
+}
+
+export default ErrorComponents
