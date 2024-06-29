46a67c60e143560826b4bfbaf103b77c1365895f
Hanhtunaung97 <littlehanhtunaung97>
2024-06-29 19:00:29 +0800

    contact add page view set up

--- src/components/Nav.components.jsx
+++ src/components/Nav.components.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavComponents = ({ children }) => {
   return (
-    <nav className=" py-3 px-2 border shadow">
+    <nav className=" py-3 px-2 border shadow mb-10 fixed w-full bg-white">
       <div className=" container mx-auto flex justify-between items-center">
         <h1 className=" font-heading text-xl font-semibold text-purple-600">Contact App</h1>
         {children}</div>
--- src/page/ContactAdd.page.jsx
+++ src/page/ContactAdd.page.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from "react";
+import { ButtonComponents, InputFormComponents } from "../components";
 
 const ContactAddPage = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    address: "",
+  });
+  const handleInputChange = (e) => {
+    setFormData((pre) => ({ ...pre, [e.target.name]: e.target.value }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
-    <div>ContactAddPage</div>
-  )
-}
+    <div className=" w-2/5 h-auto space-y-5">
+      <h1 className=" font-heading text-xl md:text-3xl font-bold text-purple-700 text-center">
+        Register Your Contact
+      </h1>
+      <form onSubmit={handleSubmit} className=" flex flex-col gap-y-5">
+        <InputFormComponents
+          onChange={handleInputChange}
+          value={formData.name}
+          type={"name"}
+          name={"name"}
+          label={"Name :"}
+          placeholder="Your Name"
+        />
+        <InputFormComponents
+          onChange={handleInputChange}
+          value={formData.email}
+          type={"email"}
+          name={"email"}
+          label={"Email :"}
+          placeholder="example@gamil.com"
+        />
+        <InputFormComponents
+          onChange={handleInputChange}
+          value={formData.phone}
+          type={"number"}
+          name={"phone"}
+          label={"Phone :"}
+          placeholder="+95979....."
+        />
+        <InputFormComponents
+          onChange={handleInputChange}
+          value={formData.address}
+          type={"text"}
+          name={"address"}
+          label={"Address :"}
+          placeholder="Your address"
+        />
+        <ButtonComponents type="submit">Register</ButtonComponents>
+      </form>
+    </div>
+  );
+};
 
-export default ContactAddPage
+export default ContactAddPage;
